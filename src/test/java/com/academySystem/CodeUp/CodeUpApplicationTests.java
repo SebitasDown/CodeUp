470d00aa4511721b3468de6b2da07c5c585bb4ea
SebitasDown <areizasebas31@gmail.com>
2025-11-06 12:52:06 -0500

    Initial commit

---
+++ src/test/java/com/academySystem/CodeUp/CodeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academySystem.CodeUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
